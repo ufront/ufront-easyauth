050e55cf5112feddc979ec224ed761f6ab88bc75
Jason O'Neil <jason.oneil@gmail.com>
2014-08-14 11:06:38 +0800

    1.0.0-rc.2

--- bundle.sh
+++ bundle.sh
@@ -3,4 +3,4 @@
 libname='ufront-easyauth'
 rm -f "${libname}.zip"
 zip -r "${libname}.zip" haxelib.json src LICENSE.txt README.md
-echo "Saved as ${libname}.zip"
+echo "Saved as ${libname}.zip - please run git tag"
